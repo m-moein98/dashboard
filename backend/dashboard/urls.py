b21fa91c1be71191e6be13b844ad4197b23d6911
mohammed moein <moein1475963.mmz@gmail.com>
2021-03-14 06:52:36 +0330

    Bugfix at /dashboard/urls

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import GetUserProfileView, UpdateProfileView
 urlpatterns = [
-    path('profile', GetUserProfileView.as_view()),
-    path('update', UpdateProfileView.as_view()),
+    path('profile', GetUserProfileView.as_view(), name="profile"),
+    path('update', UpdateProfileView.as_view(), name="update"),
 ]
