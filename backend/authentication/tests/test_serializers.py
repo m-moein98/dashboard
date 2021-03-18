221941d9b974fa293ad8c0f7be1a1d4743c25e7a
mohammed moein <moein1475963.mmz@gmail.com>
2021-03-19 00:22:25 +0330

    Bugfix at test /authentication

--- backend/authentication/tests/test_serializers.py
+++ backend/authentication/tests/test_serializers.py
@@ -6,8 +6,6 @@ from ..serializers import UserSerializer
 class TestSerializers(TestSetUp):
     def test_get_user_list(self):
         user = User.objects.create_user(username="SomeRandomUsername4Test",password="SomePasswords4Test")
-        user.save()
         created_user = User.objects.get(username="SomeRandomUsername4Test")
         serialized_data = UserSerializer(user)
-        created_user.delete()
         self.assertEqual(serialized_data.data['username'], 'SomeRandomUsername4Test', 'serializer doesnt work')
