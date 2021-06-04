cf82e0061135ba83b75fc10af6f7ee1215925f3d
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-06-04 13:04:42 +0430

    Update Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/login';
 import Register from './components/register';
 import Profile from './components/profile';
+import Navbar from './components/navbar';
+
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/* <Register /> */}
-      <Profile/>
+      <Router>
+        <Route path="/" exact>
+          <Navbar />
+          <Profile />
+        </Route>
+        <Route path="/login">
+          <Navbar />
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Navbar />
+          <Register />
+        </Route>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
